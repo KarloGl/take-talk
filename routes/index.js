26a433159ea2006ed413625c405a9578c582b429
Amar <amar.hazem@gmail.com>
2015-01-17 13:04:29 +0100

    Structure done

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET tutorial page. */
+router.get('/tutorial', function(req, res, next) {
+  res.render('tutorial.ejs');
+});
+
+/* GET documentation page. */
+router.get('/documentation', function(req, res, next) {
+  res.render('documentation');
+});
+
+/* GET create page. */
+router.get('/create', function(req, res, next) {
+  res.render('create');
+});
+
+/* GET meeting page. */
+router.get('/meeting', function(req, res, next) {
+  res.render('meeting');
+});
+
+/* GET university of Evry page. */
+router.get('*', function(req, res, next) {
+  res.redirect('www.univ-evry.fr');
+});
+
+module.exports = router;
